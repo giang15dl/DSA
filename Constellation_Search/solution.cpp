d2b57a9ff4d7c6b47955a3096ff85968d75a577f
Ngô Văn Cảnh <vancanh.ng@samsung.com>
2024-09-11 15:13:19 +0700

    template solution class

--- Constellation_Search/solution.cpp
+++ Constellation_Search/solution.cpp
@@ -5,12 +5,28 @@ struct Result {
     int y, x;
 };
 
+class Solution {
+private:
+    int N, M;
+    int (*map)[MAX_N] = {};
+
+public:
+    Solution() {}
+    Solution(int N, int M, int Map[MAX_N][MAX_N]) : N(N), M(M), map(Map) {}
+
+    Result findConstellation(int stars[MAX_M][MAX_M]) {
+        Result res;
+
+        res.y = res.x = 0;
+        return res;
+    }
+} solution;
+
+////////////////////////////////////////////////////////////////////////////////
 void init(int N, int M, int Map[MAX_N][MAX_N]) {
+    solution = Solution(N, M, Map);
 }
 
 Result findConstellation(int stars[MAX_M][MAX_M]) {
-    Result res;
-
-    res.y = res.x = 0;
-    return res;
+    return solution.findConstellation(stars);
 }
