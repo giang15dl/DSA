65384af467661b1821c6c427e07c57e45ec31b7c
Ngô Văn Cảnh <vancanh.ng@samsung.com>
2024-09-13 09:23:29 +0700

    new project: microorganism

---
+++ Microorganism/solution.cpp
@@ -0,0 +1,13 @@
+void init() {
+}
+
+void addBacteria(int tStamp, int mID, int mLifeSpan, int mHalfTime) {
+}
+
+int getMinLifeSpan(int tStamp) {
+    return 0;
+}
+
+int getCount(int tStamp, int mMinSpan, int mMaxSpan) {
+    return 0;
+}
