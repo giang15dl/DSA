b48a75993086de7298fa25f1ec39fd6487a3fd4b
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-23 15:00:54 +0700

    new problem: Toll Discount

---
+++ Toll_Discount/solution.cpp
@@ -0,0 +1,15 @@
+void init(int N, int K, int mId[], int sCity[], int eCity[], int mToll[]) {
+    return;
+}
+
+void add(int mId, int sCity, int eCity, int mToll) {
+    return;
+}
+
+void remove(int mId) {
+    return;
+}
+
+int cost(int M, int sCity, int eCity) {
+    return 0;
+}
