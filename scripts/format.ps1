30d93a0f4394d234c85dc45cd666c2151053a708
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-23 09:56:01 +0700

    add batch format scripts

---
+++ scripts/format.ps1
@@ -0,0 +1,7 @@
+$clang_format = "C:\Program Files (x86)\Microsoft Visual Studio\2017\WDExpress\Common7\IDE\VC\VCPackages\clang-format.exe"
+
+Get-ChildItem -Recurse *.cpp -Exclude .\build |
+  ForEach-Object {
+    Write-Output $_.FullName
+    #& $clang_format -i -style=file --fallback-style=LLVM $_.FullName
+  }
