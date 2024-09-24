92fdbc307af5b5e9b0f76bf9b705e8d34f8c633d
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-24 13:26:09 +0700

    add solution template

--- Microorganism/solution.cpp
+++ Microorganism/solution.cpp
@@ -1,13 +1,21 @@
-void init() {
-}
+class Solution {
+public:
+    Solution() {}
+    void addBacteria(int tStamp, int mID, int mLifeSpan, int mHalfTime) {
+    }
 
-void addBacteria(int tStamp, int mID, int mLifeSpan, int mHalfTime) {
-}
+    int getMinLifeSpan(int tStamp) {
+        return 0;
+    }
 
-int getMinLifeSpan(int tStamp) {
-    return 0;
-}
+    int getCount(int tStamp, int mMinSpan, int mMaxSpan) {
+        return 0;
+    }
+};
 
-int getCount(int tStamp, int mMinSpan, int mMaxSpan) {
-    return 0;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Solution solution;
+void init() { solution = Solution(); }
+void addBacteria(int tStamp, int mID, int mLifeSpan, int mHalfTime) { solution.addBacteria(tStamp, mID, mLifeSpan, mHalfTime); }
+int getMinLifeSpan(int tStamp) { return solution.getMinLifeSpan(tStamp); }
+int getCount(int tStamp, int mMinSpan, int mMaxSpan) { return solution.getCount(tStamp, mMinSpan, mMaxSpan); }
