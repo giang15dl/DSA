1ca236f77e1cc3e70ae9df2939b08d7171c47439
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-24 10:11:03 +0700

    Add class Solution template

--- Morning_Commute_Radio/solution.cpp
+++ Morning_Commute_Radio/solution.cpp
@@ -1,16 +1,24 @@
-void init(int N, int M, int mType[], int mTime[]) {
-}
+class Solution {
+public:
+    Solution(int N, int M, int mType[], int mTime[]) {}
+    ~Solution() {}
 
-void destroy() {
-}
+    void update(int mID, int mNewTime) {
+    }
 
-void update(int mID, int mNewTime) {
-}
+    int updateByType(int mTypeID, int mRatio256) {
+        return -1;
+    }
 
-int updateByType(int mTypeID, int mRatio256) {
-    return -1;
-}
+    int calculate(int mA, int mB) {
+        return -1;
+    }
 
-int calculate(int mA, int mB) {
-    return -1;
-}
+} *solution;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void init(int N, int M, int mType[], int mTime[]) { solution = new Solution(N, M, mType, mTime); }
+void destroy() { delete solution; }
+void update(int mID, int mNewTime) { solution->update(mID, mNewTime); }
+int updateByType(int mTypeID, int mRatio256) { return solution->updateByType(mTypeID, mRatio256); }
+int calculate(int mA, int mB) { return solution->calculate(mA, mB); }
