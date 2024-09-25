9514d756faa5faa75221a848c3a161f7578c2804
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-25 14:47:21 +0700

    add class Solution template

--- Dictionary/solution.cpp
+++ Dictionary/solution.cpp
@@ -1,31 +1,41 @@
-#define MAX_L (8)
+#define MAX_L 8
 
 struct RESULT {
     int success;
     char word[MAX_L + 1];
 };
 
-void init(int N, char mWordList[][MAX_L + 1]) {
-    return;
-}
+class Solution {
+public:
+    Solution() {}
+    Solution(int N, char mWordList[][MAX_L + 1]) {}
 
-int add(char mWord[]) {
-    return -1;
-}
+    int add(char mWord[]) {
+        return -1;
+    }
 
-int erase(char mWord[]) {
-    return -1;
-}
+    int erase(char mWord[]) {
+        return -1;
+    }
 
-RESULT find(char mInitial, int mIndex) {
-    RESULT res;
+    RESULT find(char mInitial, int mIndex) {
+        RESULT res;
 
-    res.success = -1;
-    res.word[0] = '\0';
+        res.success = -1;
+        res.word[0] = '\0';
 
-    return res;
-}
+        return res;
+    }
 
-int getIndex(char mWord[]) {
-    return -1;
-}
+    int getIndex(char mWord[]) {
+        return -1;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Solution solution;
+void init(int N, char mWordList[][MAX_L + 1]) { solution = Solution(N, mWordList); }
+int add(char mWord[]) { return solution.add(mWord); }
+int erase(char mWord[]) { return solution.erase(mWord); }
+RESULT find(char mInitial, int mIndex) { return solution.find(mInitial, mIndex); }
+int getIndex(char mWord[]) { return solution.getIndex(mWord); }
