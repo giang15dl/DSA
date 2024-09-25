672de3294561f7faa3cf81bdbdabe016ca0c3715
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-25 13:39:06 +0700

    Microorganism: solve using divide and conquer

---
+++ Microorganism/solution2.cpp
@@ -0,0 +1,129 @@
+#include <queue>
+#include <set>
+
+using namespace std;
+class Solution {
+    static const int MAX_B = 30'000 + 1;
+    static const int MAX_T = 1'000'000;
+    static const int RATE = 1000;
+    static const int MAX_S = MAX_T / RATE + 1;
+
+private:
+    struct Bacteria {
+        int id;
+        int lifeSpan;
+        int halfTime;
+        int nextHalfTime;
+        Bacteria() : id(0), lifeSpan(0), halfTime(0), nextHalfTime(0) {}
+        Bacteria(int id, int lifeSpan, int halfTime, int nextHalfTime) : id(id),
+                                                                         lifeSpan(lifeSpan),
+                                                                         halfTime(halfTime),
+                                                                         nextHalfTime(nextHalfTime) {}
+    } bacterias[MAX_B];
+
+    struct greater {
+        bool operator()(const Bacteria a, const Bacteria b) {
+            return a.nextHalfTime > b.nextHalfTime;
+        }
+    };
+
+    struct less {
+        bool operator()(const Bacteria a, const Bacteria b) const {
+            return (a.lifeSpan == b.lifeSpan) ? a.id < b.id : a.lifeSpan < b.lifeSpan;
+        }
+    };
+
+    priority_queue<Bacteria, vector<Bacteria>, greater> heapNextHalfTime;
+    set<Bacteria, less> treeLifeSpan[MAX_S];
+
+    void update(int time) {
+        while (!heapNextHalfTime.empty()) {
+            Bacteria bacteria = heapNextHalfTime.top();
+            if (bacteria.nextHalfTime > time)
+                break;
+
+            heapNextHalfTime.pop();
+            treeLifeSpan[bacteria.lifeSpan / RATE].erase(bacteria);
+
+            int nextlifespan = bacteria.lifeSpan / 2;
+
+            if (nextlifespan > 99) {
+                bacteria.lifeSpan = nextlifespan;
+                bacteria.nextHalfTime += bacteria.halfTime;
+                heapNextHalfTime.push(bacteria);
+                treeLifeSpan[nextlifespan / RATE].insert(bacteria);
+            }
+        }
+    }
+
+public:
+    Solution() {}
+    void addBacteria(int tStamp, int mID, int mLifeSpan, int mHalfTime) {
+        bacterias[mID] = Bacteria(mID, mLifeSpan, mHalfTime, tStamp + mHalfTime);
+        treeLifeSpan[mLifeSpan / RATE].insert(bacterias[mID]);
+        heapNextHalfTime.push(bacterias[mID]);
+    }
+
+    int getMinLifeSpan(int tStamp) {
+        update(tStamp);
+        for (int i = 0; i < MAX_S; i++)
+            if (!treeLifeSpan[i].empty())
+                return treeLifeSpan[i].begin()->id;
+
+        return -1;
+    }
+
+    int getCount(int tStamp, int mMinSpan, int mMaxSpan) {
+        int ans = 0;
+        update(tStamp);
+        int diff = (mMaxSpan / RATE) - (mMinSpan / RATE);
+        if (diff == 0) {
+            for (auto it : treeLifeSpan[mMinSpan / RATE]) {
+                if (it.lifeSpan > mMaxSpan) {
+                    break;
+                }
+                if (it.lifeSpan >= mMinSpan) {
+                    ans++;
+                }
+            }
+        }
+        if (diff == 1) {
+            for (auto it : treeLifeSpan[mMinSpan / RATE]) {
+                if (it.lifeSpan >= mMinSpan) {
+                    ans++;
+                }
+            }
+            for (auto it : treeLifeSpan[mMaxSpan / RATE]) {
+                if (it.lifeSpan > mMaxSpan) {
+                    break;
+                }
+                ans++;
+            }
+        }
+        if (diff > 1) {
+            for (auto it : treeLifeSpan[mMinSpan / RATE]) {
+                if (it.lifeSpan >= mMinSpan) {
+                    ans++;
+                }
+            }
+            for (auto it : treeLifeSpan[mMaxSpan / RATE]) {
+                if (it.lifeSpan > mMaxSpan) {
+                    break;
+                }
+                ans++;
+            }
+            for (int i = (mMinSpan / RATE) + 1; i < (mMaxSpan / RATE); i++) {
+                ans += treeLifeSpan[i].size();
+            }
+        }
+
+        return ans;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Solution solution;
+void init() { solution = Solution(); }
+void addBacteria(int tStamp, int mID, int mLifeSpan, int mHalfTime) { solution.addBacteria(tStamp, mID, mLifeSpan, mHalfTime); }
+int getMinLifeSpan(int tStamp) { return solution.getMinLifeSpan(tStamp); }
+int getCount(int tStamp, int mMinSpan, int mMaxSpan) { return solution.getCount(tStamp, mMinSpan, mMaxSpan); }
