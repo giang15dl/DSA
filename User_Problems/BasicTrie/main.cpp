b01dec81071bdca9a1fb9a1a6f3ea2edf0f4a625
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-25 19:16:21 +0700

    New User Problem: BasicTrie

---
+++ User_Problems/BasicTrie/main.cpp
@@ -0,0 +1,57 @@
+#include <malloc.h>
+#include <stdio.h>
+
+#define MAX_SIZE (102)
+
+////////////////////////////////////////////////////////
+////////////  TRIES ////////////////////////////////////
+
+// Initialize the Trie
+extern void init_trie();
+
+// Insert a string "key" in trie
+extern void Tries_insert(const char *key);
+
+// Returns true if whole "key" presents in trie, else false
+extern bool Tries_search(const char *key);
+
+// Returns number of keys having "key" as a prefix
+extern int Tries_CountofkeysWithPrefix(const char *key);
+
+// deletes a key from trie
+extern void Tries_deleteKey(const char *key);
+
+int main() {
+    setbuf(stdout, NULL);
+
+    freopen("input_small.txt", "r", stdin);
+
+    int T;
+    scanf("%d", &T);
+    init_trie();
+
+    for (int tc = 1; tc <= T; tc++) {
+        int cmd, val;
+        char *str;
+        str = (char *)malloc(sizeof(char) * MAX_SIZE);
+        scanf("%d %s", &cmd, str);
+
+        switch (cmd) {
+        case 0:
+            Tries_insert(str);
+            break;
+        case 1:
+            val = Tries_search(str);
+            printf("%d\n", val);
+            break;
+        case 2:
+            val = Tries_CountofkeysWithPrefix(str);
+            printf("%d\n", val);
+            break;
+        case 3:
+            Tries_deleteKey(str);
+            break;
+        }
+    }
+    return 0;
+}
---
+++ User_Problems/BasicTrie/solution.cpp
@@ -0,0 +1,113 @@
+class Trie {
+    static const int SIZE = 'z' - 'a' + 1;
+
+    struct Node {
+        int isLeaf;
+        int count;
+        Node *parent;
+        Node *child[SIZE];
+
+        Node() : isLeaf(false), count(0), parent(nullptr) {
+            for (int i = 0; i < SIZE; i++)
+                this->child[i] = nullptr;
+        }
+    };
+
+    void clear(Node *root) {
+        for (int i = 0; i < SIZE; i++)
+            if (root->child[i])
+                clear(root->child[i]);
+        delete root;
+    }
+
+    Node *root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    ~Trie() {
+        clear(root);
+    }
+
+    void insert(const char *key) {
+        Node *node = root;
+        while (*key != '\0') {
+            node->count++;
+            int index = *key++ - 'a';
+            if (!node->child[index]) {
+                node->child[index] = new Node();
+                node->child[index]->parent = node;
+            }
+            node = node->child[index];
+        }
+        node->isLeaf++;
+        node->count++;
+    }
+
+    bool find(const char *key) {
+        Node *node = root;
+        while (*key != '\0') {
+            int index = *key++ - 'a';
+            if (!node->child[index] || !node->child[index]->count)
+                return 0;
+            node = node->child[index];
+        }
+        if (!node->isLeaf)
+            return 0;
+        return 1;
+    }
+
+    int count(const char *key) {
+        Node *node = root;
+        while (*key != '\0') {
+            int index = *key++ - 'a';
+            if (!node->child[index] || !node->child[index]->count)
+                return 0;
+            node = node->child[index];
+        }
+        return (node->count);
+    }
+
+    void erase(const char *key) {
+        Node *node = root;
+        while (*key != '\0') {
+            int index = *key++ - 'a';
+            if (!node->child[index] || !node->child[index]->count)
+                return;
+            node = node->child[index];
+        }
+        if (node->isLeaf) {
+            node->isLeaf--;
+            while (node != nullptr) {
+                node->count--;
+                node = node->parent;
+            }
+        }
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Trie trie;
+
+void init_trie() {
+    trie = Trie();
+}
+
+void Tries_insert(const char *key) {
+    trie.insert(key);
+}
+
+bool Tries_search(const char *key) {
+    return trie.find(key);
+}
+
+int Tries_CountofkeysWithPrefix(const char *key) {
+    return trie.count(key);
+}
+
+void Tries_deleteKey(const char *key) {
+    trie.erase(key);
+}
